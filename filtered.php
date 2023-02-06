028983c11635cb9243a69718dfb5a01459204f54
NicolaPlacentino <nyko0921@gmail.com>
2023-02-06 15:37:46 +0100

    first commit

---
+++ filtered.php
@@ -0,0 +1,37 @@
+<!-- Creare un form che permetta di inviare due campi: un paragrafo ed una parola da censurare.
+Gestire il tutto con due file diversi.
+Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.
+Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
+- stampare a schermo il paragrafo e la sua lunghezza
+- stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare -->
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Nicola Placentino">
+    <title>Bad Words</title>
+</head>
+
+<body>
+    <h1>Bad Words</h1>
+
+    <h3>Il tuo paragrafo originale:</h3>
+    <p></p>
+    <h6>Lunghezza paragrafo:</h6>
+    
+    <hr>
+
+    <h3>Il paragrafo censurato:</h3>
+    <p></p>
+    <h6>Lunghezza paragrafo:</h6>
+
+</body>
+
+</html>
